ba8cf8834d0fbcad32ce584abc11a5af5aefc587
Jung Liu <realumbrally@gmail.com>
2020-05-06 13:05:57 +0800

    add getHistoricalMsgs and addMsg function

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,10 +1,26 @@
 const db = require('../models')
 const Msg = db.Msg
+const User = db.User
 
 const chatController = {
   getHistoricalMsgs: (req, res) => {
-
-    return res.send('歷史資訊')
+    Msg.findAll({ include: User }).then(
+      results => {
+        return res.json({
+          historicalMsgs: results
+        })
+      }
+    )
+  },
+  addMsg: (req, res) => {
+    Msg.create({
+      content: req.body.msg,
+      UserId: 1
+    }).then(msg => {
+      res.json(
+        { status: 'success', message: msg }
+      )
+    })
   }
 }
 module.exports = chatController
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/userController.js')
 module.exports = app => {
   app.get('/chat', chatController.getHistoricalMsgs
   )
+  app.post('/chat', chatController.addMsg)
   app.post('/signin', userController.signIn)
   app.post('/signup', userController.signUp)
 }
